2da02db60150962dab19d7bf764f58ce1cdf9e30
A. Andrade dos Santos <a.andrade.dos.santos@avanade.com>
2020-03-11 22:38:59 -0300

    add hashed password validation

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,5 @@
 const userCollection = require('../model/userSchema')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 
 const signUp = (req, res) => {
     const cryptedPswd = bcrypt.hashSync(req.body.senha)
@@ -9,7 +8,10 @@ const signUp = (req, res) => {
 
     addNewUser.save((err) => {
         if(err){
-            return res.status(400).send(err)
+            return res.status(400).json(
+                {
+                    erro: "E-mail já existente"
+                })
         } else {
             return res.status(201).send(addNewUser)
         }
@@ -20,16 +22,15 @@ const signIn = async (req, res) => {
         const email = req.body.email
         const senha = req.body.senha
         const user = await userCollection.findOne({ email })
-        const validEmail = bcrypt.compareSync(senha, user.senha)
+        const validPswd = bcrypt.compareSync(senha, user.senha)
 
-        if (validPswd) {
+        if(validPswd) {
           return res.status(200).send(user)
         }
-          return res.status(401).send(
+          return res.status(400).json(
         {
-            error: "Usuário e/ou senha inválidos"
+            erro: "Usuário e/ou senha inválidos"
         })
-
     }
 
 module.exports = { 
