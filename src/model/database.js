0e752e377f2b15acabc69afa1bfa972ecc219dda
A. Andrade dos Santos <a.andrade.dos.santos@avanade.com>
2020-03-11 22:41:20 -0300

    add database connection

--- src/model/database.js
+++ src/model/database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-
 const DB_URL  = 'mongodb://localhost:27017/SkyDB'
 const connect = () => {
     mongoose.connect(DB_URL, {
@@ -11,7 +10,7 @@ const connect = () => {
 const connection = mongoose.connection
 connection.on('error',() => console.error('Erro ao conectar no banco.'))
 
-connection.once('open', () => console.log('Estamos conectados.'))
+connection.once('open', () => console.log('Estamos conectados ao MongoDB.'))
 }
 
 module.exports = {
