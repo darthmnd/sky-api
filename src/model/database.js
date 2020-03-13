d7a9b34b12b0fd4555214a71861ff3a91acae6dd
A. Andrade dos Santos <a.andrade.dos.santos@avanade.com>
2020-03-13 11:05:22 -0300

    add .env archives and MONGODB_URI

--- src/model/database.js
+++ src/model/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const DB_URL  = 'mongodb://localhost:27017/SkyDB'
+const DB_URL  = process.env.MONGODB_URI || 'mongodb://localhost:27017/SkyDB'
 const connect = () => {
     mongoose.connect(DB_URL, {
         useNewUrlParser:true,
